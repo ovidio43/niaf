3a8bc0e01ab3db28d87e0994fc165c19ff1b83a8
edwin condori <ovidio43@gmail.com>
2014-01-24 08:27:51 -0400

    edit form

---
+++ wp-content/themes/Themeniaft/include/form-give_the_gift_of_heritage.php
@@ -0,0 +1,3 @@
+<form action="<?php echo get_template_directory_uri(); ?>/include/send-form-new-york-gala-registration.php" method="post" name="form-new-york" id="form-new-york">
+  
+</form>
---
+++ wp-content/themes/Themeniaft/template-form-give_the_gift_of_heritage.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template name: gala registration form
+ *
+ * @package WordPress
+ * @subpackage themeniaft
+ * @since theme niaft 1.0
+ */
+get_header();
+?>
+
+<div class="main-container">
+    <div class="main clearfix">
+        <div class="primary">
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header>
+                <div class="entry-content">
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php the_content();?>
+                    <?php endwhile; // end of the loop. ?>
+                    
+                    <div class="wrap-form">
+                        <?php require_once (get_template_directory() . '/include/form-new-york-gala-registration.php');?>
+                    </div>
+                </div><!-- .entry-content -->
+            </article><!-- #post -->        
+        </div>
+        <aside class="sidebar">
+            <?php get_sidebar(); ?>
+        </aside>
+    </div> <!-- #main -->
+</div> <!-- #main-container -->
+
+<?php get_footer(); ?>
