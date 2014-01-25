a2caabbce13d324551e75941f19ae27a80c1b452
edwin condori <ovidio43@gmail.com>
2014-01-25 15:44:21 -0400

    edit sc

--- wp-content/themes/Themeniaft/single.php
+++ wp-content/themes/Themeniaft/single.php
@@ -11,26 +11,34 @@ global $current_user;
     get_currentuserinfo();
     $type = 'member-area';
     if($current_user->ID!=null){
-        $args=array(
-            'post_type' => array( 'member-area'),
-            'post_status' => 'publish',
-            'orderby' => 'date',
-            'order' => 'DESC',
-            'posts_per_page'=>-1,
-            'p'=>get_the_ID(),
-            'meta_query' => array(
-                 array(
-                    'key' => "wpcf-level-$current_user->user_lastname",
-                    'value' => strtoupper($current_user->user_lastname),
-                    'type' => 'STRING',
-                    'compare' => '='
+        if(get_post_type()=="member-area"){
+            $args=array(
+                'post_type' => array( 'member-area'),
+                'post_status' => 'publish',
+                'orderby' => 'date',
+                'order' => 'DESC',
+                'posts_per_page'=>-1,
+                'p'=>get_the_ID(),
+                'meta_query' => array(
+                     array(
+                        'key' => "wpcf-level-$current_user->user_lastname",
+                        'value' => strtoupper($current_user->user_lastname),
+                        'type' => 'STRING',
+                        'compare' => '='
+                    )
                 )
-            )
-        );
+            );            
+        }else{
+            $args=array(
+                'post_type' => array( 'post', 'home-slider','niaf-partner','news-events','niaf-featured','niaf-highlights','ai1ec_event'),
+                'p'=>get_the_ID()
+            );
+        }
+ 
     }else{
         $args=array(
-            'p'=>get_the_ID(),
-            'post_type' => array( 'post', 'home-slider','niaf-partner','news-events','niaf-featured','niaf-highlights','ai1ec_event')
+            'post_type' => array( 'post', 'home-slider','niaf-partner','news-events','niaf-featured','niaf-highlights','ai1ec_event'),
+            'p'=>get_the_ID()
         );  
     }
     $myposts = new WP_Query( $args );
