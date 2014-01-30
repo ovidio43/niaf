bdf4f11d93b3b48cc64ba98f126b7c8af79ad419
edwin condori <ovidio43@gmail.com>
2014-01-30 08:01:14 -0400

    edit style

---
+++ wp-content/themes/Themeniaft/page-calendar-events.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+ * The page containing .
+ *
+ * @package WordPress
+ * @subpackage themeniaft
+ * @since theme niaft 1.0
+ */
+get_header(); ?>
+
+        <div class="main-container">
+            <div class="main clearfix">
+				<div class="primary">
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php get_template_part( 'content', 'page' ); ?>
+                        <div class="wraptextevent">
+                            <?php the_field('text_below_full_calendar_events'); ?>
+                        </div>
+                    <?php endwhile; // end of the loop. ?>
+                </div>
+                <aside class="sidebar">
+                    <?php get_sidebar(); ?>
+                </aside>
+    </div> <!-- #main -->
+</div> <!-- #main-container -->
+
+<?php get_footer(); ?>
+   
