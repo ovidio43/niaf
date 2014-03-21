06e135b282a736521d0d3f8b607ec59608d7438d
aldoadachi <aldoadachi@altra.com.bo>
2014-03-21 09:40:59 -0400

    edit

--- wp-content/themes/Themeniaft/template-news-release-archive.php
+++ wp-content/themes/Themeniaft/template-news-release-archive.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Template name: Archive news and releases
  *
@@ -6,58 +6,92 @@
  * @subpackage themeniaft
  * @since theme niaft 1.0
  */
-get_header(); ?>
+get_header();
+?>
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        jQuery('select#filter').on('change', function() {
+            jQuery('form#formFilter').submit();
+        });
+    });
+</script>
 <div class="main-container">
-  <div class="main clearfix">
-      <div class="primary ">
-          <?php 
-          if ( have_posts() ) : ?>      
-          <?php while ( have_posts() ) : the_post(); ?> 
-                <header class="entry-header">
-                  <h1 class="entry-title">
-                  <?php 
-                    the_title();
-                  ?>
-                  </h1>
-                </header> 
-                <?php the_content();?>       
-          <?php   endwhile; wp_reset_postdata();?>
-          <?php 
-          endif;
-          ?>          
-          <?php
-          $today = strtotime(date('Ymd'));
-          $args=array(
-              'post_type' => 'niaf_event',
-              'post_status' => 'publish',
-              'meta_key' => 'date_niaf_event_publish',
-              'orderby' => 'meta_value', 
-              'order' => 'ASC',
-              'offset' => 0,
-              'posts_per_page'=>-1,
-              'date_query' => array(
-                array(
-                  'year'      => date('Y'),
-                  'compare'   => '<',
-                )
-              )            
-            );          
-          $myposts = new WP_Query( $args );
-          if ( $myposts->have_posts() ) : ?>
-              <?php while ( $myposts->have_posts() ) : $myposts->the_post(); 
-              ?> 
-                  <?php get_template_part( 'content', 'taxonomy' ); ?>
-              <?php endwhile;?>
+    <div class="main clearfix">
+        <div class="primary ">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+                    ?> 
+                    <header class="entry-header">
+                        <h1 class="entry-title">
+                            <?php
+                            the_title();
+                            ?>
+                            <form id="formFilter" action="" method="get" style="float: right;font-size: 15px;padding: 10px;">
+                                <label for="filter">Filter : </label><select name="filter" id="filter" >
+                                    <?php
+                                    $year = Array('2013', '2012');
+                                    foreach ($year as $value) {
+                                        if ($_GET['filter'] == $value) {
+                                            ?>
+                                            <option value="<?php echo $value; ?>" selected><?php echo $value; ?></option>
+                                            <?php
+                                        } else {
+                                            ?>
+                                            <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
+                                            <?php
+                                        }
+                                    }
+                                    ?>                                    
+                                </select>
+                            </form>
+                        </h1>
 
-          <?php wp_reset_query(); 
-          endif;
-          ?>  
-      </div>
-      <aside class="sidebar">
-          <?php get_sidebar(); ?>
-      </aside>
-  </div> <!-- #main -->
+                    </header> 
+                    <?php
+                    the_content();
+                endwhile;
+                wp_reset_postdata();
+                ?>
+                <?php
+            endif;
+            ?>          
+            <?php
+            $y = $_GET['filter'];
+            if (date('Y') == $y) {
+                $y = (date('y') - 1);
+            }
+            $today = strtotime(date('Ymd'));
+            $args = array(
+                'post_type' => 'niaf_event',
+                'post_status' => 'publish',
+                'year' => $y,
+                'meta_key' => 'date_niaf_event_publish',
+                'orderby' => 'meta_value',
+                'order' => 'ASC',
+                'offset' => 0,
+                'posts_per_page' => -1,
+                'date_query' => array(
+                    array(
+                        'year' => date('Y'),
+                        'compare' => '<',
+                    )
+                )
+            );
+            $myposts = new WP_Query($args);
+            if ($myposts->have_posts()) :
+                while ($myposts->have_posts()) : $myposts->the_post();
+                    get_template_part('content', 'taxonomy');
+                endwhile;
+                wp_reset_query();
+            endif;
+            ?>  
+        </div>
+        <aside class="sidebar">
+            <?php get_sidebar(); ?>
+        </aside>
+    </div> <!-- #main -->
 </div> <!-- #main-container -->
 
-<?php get_footer(); ?>
-   
+<?php
+get_footer();
--- wp-content/themes/Themeniaft/template-news_releases.php
+++ wp-content/themes/Themeniaft/template-news_releases.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Template name: display news and releases
  *
@@ -6,53 +6,55 @@
  * @subpackage themeniaft
  * @since theme niaft 1.0
  */
-get_header(); ?>
+get_header();
+?>
 <div class="main-container">
-  <div class="main clearfix">
-      <div class="primary ">
-          <?php while ( have_posts() ) : the_post(); ?> 
-                  <header class="entry-header">
+    <div class="main clearfix">
+        <div class="primary ">
+            <?php while (have_posts()) : the_post(); ?> 
+                <header class="entry-header">
                     <h1 class="entry-title">
-                    <?php 
-                      the_title();
-                    ?>
+                        <?php
+                        the_title();
+                        ?>
                     </h1>
-                  </header> 
-                  <?php $content=get_the_content();?>       
-          <?php endwhile;?>
-          <?php
-          wp_reset_query(); 
-          $today = strtotime(date('Ymd'));
-          $args=array(
-              'post_type' => 'niaf_event',
-              'post_status' => 'publish',
-              'year' => date('Y'),
-              'meta_key' => 'date_niaf_event_publish',
-              'orderby' => 'meta_value',               
-              'order' => 'DESC',
-              'posts_per_page'=>-1
-            );          
-          $myposts = new WP_Query( $args );
-          if ( $myposts->have_posts() ) : ?>   
-              <?php while ( $myposts->have_posts() ) : $myposts->the_post(); ?> 
+                </header> 
+                <?php $content = get_the_content(); ?>       
+            <?php endwhile; ?>
+            <?php
+            wp_reset_query();
+            $today = strtotime(date('Ymd'));
+            $args = array(
+                'post_type' => 'niaf_event',
+                'post_status' => 'publish',
+                'year' => date('Y'),
+                'meta_key' => 'date_niaf_event_publish',
+                'orderby' => 'meta_value',
+                'order' => 'DESC',
+                'posts_per_page' => -1
+            );
+            $myposts = new WP_Query($args);
+            if ($myposts->have_posts()) :
+                ?>   
+                <?php while ($myposts->have_posts()) : $myposts->the_post(); ?> 
 
-                  <?php 
-                  get_template_part( 'content', 'taxonomy' ); 
+                    <?php
+                    get_template_part('content', 'taxonomy');
+                    ?>
+                <?php endwhile; ?>
 
-                  ?>
-              <?php endwhile;?>
-
-          <?php wp_reset_query(); 
+                <?php
+                wp_reset_query();
             endif;
-          ?>  
+            ?>  
 
-                  <?php echo $content;?>       
-        
-      </div>
-      <aside class="sidebar">
-          <?php get_sidebar(); ?>
-      </aside>
-  </div> <!-- #main -->
+            <?php echo $content; ?>       
+
+        </div>
+        <aside class="sidebar">
+            <?php get_sidebar(); ?>
+        </aside>
+    </div> <!-- #main -->
 </div> <!-- #main-container -->
 
 <?php get_footer(); ?>
